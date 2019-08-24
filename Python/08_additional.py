29c6dd6f0a7c10613277f563bd792201f99838b2
Nebojša Koturović <necer95@gmail.com>
2019-08-25 00:05:22 +0200

    VI jun2_2019 - 1. zadatak

---
+++ Python/08_additional.py
@@ -0,0 +1,6 @@
+# Get the ASCII number of a character
+number = ord('A')
+# Get the character given by an ASCII number
+char = chr(65)
+alphabet = [chr(c) for c in range(ord('A'), ord('Z')+1)]
+print(chars)
