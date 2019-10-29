87fd8633e99f13d20452b4749848b74880778f2d
Nebojša Koturović <necer95@gmail.com>
2019-10-29 15:19:38 +0100

    Haskell: toFraction and !-! signature fix

--- Haskell/examples/fraction/src/Fraction.hs
+++ Haskell/examples/fraction/src/Fraction.hs
@@ -24,13 +24,13 @@ data FractionException
 
 instance Exception FractionException
 -----------------------------------
-toFraction :: (Integral t, Num t, Show t) => t -> t -> Fract t
+toFraction :: (Integral t) => t -> t -> Fract t
 toFraction n d =  
     case d of
         0 -> throw FractionZeroDenominator
         _ -> simplify (n :-: d)
 
-(!-!) :: (Integral t, Num t, Show t) => t -> t -> Fract t
+(!-!) :: (Integral t) => t -> t -> Fract t
 (!-!) = toFraction
 
 fromPair :: (Integral t, Num t, Show t) => (t, t) -> Fract t
@@ -53,7 +53,7 @@ simplify (n :-: d) =
 instance (Show t) => Show (Fract t) where
     show (n :-: d) = show n ++ "/" ++ show d
 
-instance (Show t, Integral t, Num t) => Num (Fract t) where
+instance (Integral t, Num t) => Num (Fract t) where
     (n1 :-: d1) + (n2 :-: d2) = ((n1 * d2) + (n2 * d1)) !-! (d1 * d2)
     (n1 :-: d1) - (n2 :-: d2) = ((n1 * d2) - (n2 * d1)) !-! (d1 * d2)
     (n1 :-: d1) * (n2 :-: d2) = (n1 * n2) !-! (d1 * d2)
