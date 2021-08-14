39e0a0431c0e3f7f540ab7266f051a5d4c87e9be
Nebojša Koturović <necer95@gmail.com>
2021-08-14 14:04:16 +0200

    Modify c++/useful_examples/08

--- c++/usefull_examples/08_sfinae_concepts/main.cpp
+++ c++/usefull_examples/08_sfinae_concepts/main.cpp
@@ -4,28 +4,33 @@
 // There is alternative to SFINAE - tag dispatch, hidden from user 
 // Another alternative may be strong types in some cases
 
+// Helper function
+template <bool condition=false>
+inline consteval void static_check() { static_assert(condition); };
+
+
 #define SFINAE_EX 1
 
 // https://en.cppreference.com/w/cpp/feature_test
 #ifndef __cpp_lib_concepts
     #if SFINAE_EX == 1 // enable_if in template argument
-        template<typename T, std::enable_if_t<std::is_integral<T>::value, bool> = true>
+        template<typename T, std::enable_if_t<std::is_integral_v<T>, bool> = true>
         T f(T t) {
             return t * t;
         }
 
-        template<typename T, std::enable_if_t<std::is_floating_point<T>::value, bool> = true>
+        template<typename T, std::enable_if_t<std::is_floating_point_v<T>, bool> = true>
         T f(T t) {
             return t * t * t;
         }
     #elif SFINAE_EX == 2 // enable_if in return type
         template<typename T>
-        std::enable_if_t<std::is_integral<T>::value, T> f(T t) {
+        std::enable_if_t<std::is_integral_v<T>, T> f(T t) {
             return t * t;
         }
 
         template<typename T>
-        std::enable_if_t<std::is_floating_point<T>::value, T> f(T t) {
+        std::enable_if_t<std::is_floating_point_v<T>, T> f(T t) {
             return t * t * t;
         }
     #elif SFINAE_EX == 3 // enable_if in function argument
@@ -33,18 +38,38 @@
         // it's ambigous but, is_integral<T> and is_floating<T> are two
         // different classes, we could use !is_integral<T> it would also work
         template<typename T>
-         // same as: std::enable_if_t<std::is_integral<T>::value, void> *
+         // same as: std::enable_if_t<std::is_integral_v<T>, void> *
          // is just common to use: void * = nullptr 
          // because it's rarely missused
-         T f(T t, std::enable_if_t<std::is_integral<T>::value>* = nullptr) {
+         T f(T t, std::enable_if_t<std::is_integral_v<T>>* = nullptr) {
             return t * t;
         }
 
         template<typename T>
-       T f(T t, std::enable_if_t<std::is_floating_point<T>::value>* = nullptr) {
+       T f(T t, std::enable_if_t<std::is_floating_point_v<T>>* = nullptr) {
             return t * t * t;
         }
     #endif
+
+    // Alternative (all inside single function)
+    template <class T>
+    T g(T t) {
+        if constexpr (std::is_integral_v<T>) {
+            return t * t;
+        } else if constexpr (std::is_floating_point_v<T>) {
+            return t * t * t;
+        } else {
+            // cpp20
+            // []<bool c = false> { static_assert(c, "no match"); }();
+
+            static_check<std::is_floating_point_v<T> || std::is_integral_v<T>>();
+            // static_assert(std::is_floating_point_v<T> 
+            //                || std::is_integral_v<T>, 
+            //               "Type not supported");
+
+        }
+    }
+
 #else // c++20 concepts version
     #include <concepts>
 
@@ -58,13 +83,28 @@
 
     template <Floating T>
     T f(T t) {
-        return t * t;
+        return t * t * t;
+    }
+
+    // Alternative (all inside single function)
+    template <class T>
+    T g(T t) {
+        if constexpr (std::is_integral_v<T>) {
+            return t * t;
+        } else if constexpr (Floating<T>) {
+            return t * t * t;
+        } else {
+            []<bool c = false>() { static_assert(c, "Invalid type"); }();
+        }
     }
 #endif
+
 
 int main() {
     std::cout << f(2u) << '\n';
     std::cout << f(23) << '\n';
     std::cout << f(2.2) << '\n';
     std::cout << f(2.12f) << '\n';
+    std::cout << g(2.12f) << '\n';
+    //std::cout << g("h") << '\n';
 }
